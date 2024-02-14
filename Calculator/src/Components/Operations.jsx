aea7161fb79a765ed8b83b4fa05fd4ae3f36aa06
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-02-14 22:02:44 +0530

    completed

---
+++ Calculator/src/Components/Operations.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ACTIONS } from '../App'
+export default function Operations({ dispatch, operation }) {
+  return (
+    <div>    
+    <button
+    onClick={()=>dispatch({type:ACTIONS.CHOOSE_OPERATION, payload: {operation}})}> 
+     {operation}
+    </button>
+    </div>
+
+  )
+}
